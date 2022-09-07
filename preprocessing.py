eb7c972a3e835f2db5b978cf9afea7cc9ac5f7b6
Xiaoda-Zhong <84829831+Xiaoda-Zhong@users.noreply.github.com>
2022-09-08 01:03:39 +0800

    Add files via upload

---
+++ preprocessing.py
@@ -0,0 +1,57 @@
+import json
+from torch.utils.data import Dataset
+import torch
+from transformers import AutoTokenizer
+from datasets import load_dataset
+
+
+train_data = "datasets_processed/train.json"
+# test_data = "datasets_processed/test.json"
+
+
+# 定义 Dataset
+class EEPLDataset(Dataset):
+    def __init__(self, path_to_file, split):
+        dataset = load_dataset(path='json', data_files=path_to_file,
+                               split=split)
+
+        def f(data):
+            return len(data['prompt']) < 300
+
+        self.dataset = dataset.filter(f)
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
+
+        text = tokenizer(self.dataset[idx]["text"], padding='max_length', max_length=300, truncation=True, return_tensors='pt')
+        prompt = tokenizer(self.dataset[idx]["prompt"], padding='max_length', max_length=300, truncation=True, return_tensors='pt')
+        event_type = tokenizer(self.dataset[idx]["event_type"], padding='max_length', max_length=300, truncation=True, return_tensors='pt')
+        trigger = tokenizer(self.dataset[idx]["trigger"], padding='max_length', max_length=300, truncation=True, return_tensors='pt')
+        event_label = self.dataset[idx]["event_label"]
+        labels = self.dataset[idx]["labels"]
+        sample = {"text": text, "prompt": prompt, "event_type": event_type, "trigger": trigger, "event_label": event_label, "labels": labels}
+
+        # 返回一个 dict
+        return sample
+
+
+trainset = EEPLDataset(train_data, split='train')
+print(len(trainset))
+train_size = int(0.8 * len(trainset))
+eval_size = len(trainset) - train_size
+train_dataset, eval_dataset = torch.utils.data.random_split(trainset, [train_size, eval_size])
+
+
+
+'''
+train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True, num_workers=0)
+
+for i, batch in enumerate(train_loader):
+    if i == 0:
+        inputbatch = batch
+        break
+        # print(batch)
+'''
---
+++ process_data.py
@@ -0,0 +1,55 @@
+import _pickle as pickle
+import jsonlines
+from transformers import BertTokenizer, BertModel
+import torch
+
+def process_train(file):
+    fr = open(file, 'rb')  # open的参数是pkl文件的路径
+    data = pickle.load(fr)  # 读取pkl文件的内容
+
+    newfile = jsonlines.open("datasets_processed/train.json", "w")
+
+    event_types = ['INJURE', 'DECLARE_BANKRUPTCY', 'SENTENCE', 'TRIAL_HEARING', 'DEMONSTRATE', 'START_POSITION',
+                   'TRANSPORT', 'ATTACK', 'EXECUTE', 'DIE', 'END_POSITION', 'NOMINATE', 'TRANSFER_OWNERSHIP', 'FINE',
+                   'ACQUIT', 'ARREST_JAIL', 'TRANSFER_MONEY', 'PHONE_WRITE', 'START_ORG', 'BE_BORN', 'SUE', 'CONVICT',
+                   'MERGE_ORG', 'END_ORG', 'ELECT', 'MARRY', 'EXTRADITE', 'DIVORCE', 'CHARGE_INDICT', 'MEET',
+                   'RELEASE_PAROLE', 'APPEAL', 'PARDON']
+
+    tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+    for i, sample in enumerate(data):
+        newsample = {}
+        newsample['text'] = sample['sentence']
+        newsample['event_type'] = sample['sentence_type']
+        newsample['trigger'] = sample['sentence_keyword']
+        newsample['prompt'] = sample['sentence'] + ' ' + '[SEP] The text is talking about' + ' ' + sample['sentence_type'] + '. The trigger is [MASK].'
+
+        trigger_token = tokenizer(newsample['trigger'], return_tensors='pt', add_special_tokens=False).to(device)
+        trigger_ids = trigger_token["input_ids"].to(device)  # 1*n
+        trigger_ids = trigger_ids.squeeze(0)  # n
+
+        newsample['labels'] = trigger_ids.item()
+
+        for j, type in enumerate(event_types):
+            if newsample['event_type'] == type:
+                newsample['event_label'] = j
+
+        jsonlines.Writer.write(newfile, newsample)
+    newfile.close()
+
+'''
+def process_test(file):
+'''
+
+
+
+
+
+
+
+
+
+'''------------processing----------------------'''
+process_train('datasets/nyt_train_data.pickle')
